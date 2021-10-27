eba407e21dbe26d1e3fcc86dcc94fd2e71858e91
jeong-inho <derosatam76@gmail.com>
2021-10-27 23:44:17 +0900

    feat: RequestBody 클래스에 of 메소드 생성

--- src/main/java/request/RequestBody.java
+++ src/main/java/request/RequestBody.java
@@ -1,6 +1,12 @@
 package request;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URLDecoder;
 import java.util.Map;
+
+import static util.HttpRequestUtils.parseValues;
+import static util.IOUtils.readData;
 
 public class RequestBody {
 
@@ -8,5 +14,13 @@ public class RequestBody {
 
     private RequestBody(Map<String, String> bodies) {
         this.bodies = bodies;
+    }
+
+    public static RequestBody of(BufferedReader bufferedReader, RequestHeader headers) throws IOException {
+        if (!headers.containsKey("Content-Length")) {
+            return null;
+        }
+        String body = URLDecoder.decode(readData(bufferedReader, headers.getContentLength()), "UTF-8");
+        return new RequestBody(parseValues(body, "&"));
     }
 }
