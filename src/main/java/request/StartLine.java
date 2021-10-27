f5bfe4949ae738ee12648b345f068b53ed68153a
jeong-inho <derosatam76@gmail.com>
2021-10-27 23:43:31 +0900

    feat: StartLine 클래스에 of 메소드 생성

--- src/main/java/request/StartLine.java
+++ src/main/java/request/StartLine.java
@@ -2,15 +2,38 @@ package request;
 
 import util.status.HttpMethod;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class StartLine {
 
-    private HttpMethod httpMethod;
-    private String requestTarget;
-    private String httpVersion;
+    private final HttpMethod httpMethod;
+    private final String requestTarget;
+    private final String httpVersion;
 
-    public StartLine(HttpMethod httpMethod, String requestTarget, String httpVersion) {
+    private StartLine(HttpMethod httpMethod, String requestTarget, String httpVersion) {
         this.httpMethod = httpMethod;
         this.requestTarget = requestTarget;
         this.httpVersion = httpVersion;
+    }
+
+    public static StartLine of(BufferedReader bufferedReader) throws IOException {
+        String[] splitStartLine = bufferedReader.readLine().split(" ");
+        HttpMethod httpMethod = HttpMethod.valueOf(splitStartLine[0]);
+        String requestTarget = splitStartLine[1];
+        String httpVersion = splitStartLine[2];
+        return new StartLine(httpMethod, requestTarget, httpVersion);
+    }
+
+    public HttpMethod getHttpMethod() {
+        return httpMethod;
+    }
+
+    public String getRequestTarget() {
+        return requestTarget;
+    }
+
+    public String getHttpVersion() {
+        return httpVersion;
     }
 }
