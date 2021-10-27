561cf0d4ee3d3a123cf8d7a26ca4e302937bc125
jeong-inho <derosatam76@gmail.com>
2021-10-27 23:43:55 +0900

    feat: RequestHeader 클래스에 of 메소드 생성

--- src/main/java/request/RequestHeader.java
+++ src/main/java/request/RequestHeader.java
@@ -1,6 +1,13 @@
 package request;
 
+import util.HttpRequestUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+
+import static util.HttpRequestUtils.parseHeader;
 
 public class RequestHeader {
 
@@ -8,5 +15,28 @@ public class RequestHeader {
 
     private RequestHeader(Map<String, String> headers) {
         this.headers = headers;
+    }
+
+    public static RequestHeader of(BufferedReader bufferedReader) throws IOException {
+        Map<String, String> headers = new HashMap<>();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            if (line.equals("")) {
+                break;
+            }
+            if (line.contains(": ")) {
+                HttpRequestUtils.Pair pair = parseHeader(line);
+                headers.put(pair.getKey(), pair.getValue());
+            }
+        }
+        return new RequestHeader(headers);
+    }
+
+    public boolean containsKey(String key) {
+        return headers.containsKey(key);
+    }
+
+    public int getContentLength() {
+        return Integer.parseInt(headers.get("Content-Length"));
     }
 }
