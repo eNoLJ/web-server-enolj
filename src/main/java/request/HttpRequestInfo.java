e5fd1c21a9cf9e3934466ba0530bf2b5c58b1289
jeong-inho <derosatam76@gmail.com>
2021-10-27 23:45:37 +0900

    feat: HttpRequestInfo 클래스에 생성자와 getHttpMethod(), getRequestTarget() 메소드 생성

--- src/main/java/request/HttpRequestInfo.java
+++ src/main/java/request/HttpRequestInfo.java
@@ -1,6 +1,9 @@
 package request;
 
+import util.status.HttpMethod;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 
 public class HttpRequestInfo {
 
@@ -8,7 +11,17 @@ public class HttpRequestInfo {
     private RequestHeader requestHeader;
     private RequestBody requestBody;
 
-    public HttpRequestInfo(BufferedReader bufferedReader) {
+    public HttpRequestInfo(BufferedReader bufferedReader) throws IOException {
+        this.startLine = StartLine.of(bufferedReader);
+        this.requestHeader = RequestHeader.of(bufferedReader);
+        this.requestBody = RequestBody.of(bufferedReader, requestHeader);
+    }
 
+    public HttpMethod getHttpMethod() {
+        return startLine.getHttpMethod();
+    }
+
+    public String getRequestTarget() {
+        return startLine.getRequestTarget();
     }
 }
